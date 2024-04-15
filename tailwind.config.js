89edbdf69b9b5ddb7114dc89fdf014dc3cececaa
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-04-14 21:35:33 -0300

    css and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        eGray: "#2a2927",
+      },
+    },
   },
   plugins: [],
 };
